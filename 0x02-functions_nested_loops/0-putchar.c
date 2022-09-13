fe17d1eec0b17cc5530697375a05cb9ea8ecd56a
Danizony <danzin2000@example.com>
2022-09-13 15:08:32 -0700

    putchar file

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,6 @@
 #include "main.h"
-
 /**
  * main - print _putchar
- * 
  * Return: 0
  */
 int main(void)
@@ -15,6 +13,6 @@ int main(void)
 	_putchar('h');
 	_putchar('a');
 	_putchar('r');
-	_putchar('/n');
+	_putchar('\n');
 	return (0);
 }
