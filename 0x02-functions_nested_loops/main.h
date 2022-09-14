4c02602576c71a28c5f94acd8b76ac31e670bf87
Danizony <danzin2000@example.com>
2022-09-13 17:31:21 -0700

    is lower file

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,8 +2,9 @@
 /**
  *File:main.h
  *Desc: header file containing functions declaration
- */
+*/
 
 int _putchar(char);
 void print_alphabet(void);
 void print_alphabet_x10(void);
+int _islower(int c);
