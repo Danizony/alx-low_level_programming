7407edba660ff585b5b1ad6d72a822527b802dcb
Danizony <danzin2000@example.com>
2022-09-19 19:58:55 -0700

    part of a string

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -15,4 +15,8 @@ void print_rev(char *s);
 int _putchar(char c);
 void rev_string(char *s);
 void swap_char(char *a, char *b);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
 #endif /*MAIN_H*/
