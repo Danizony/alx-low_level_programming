8eb15bca3aca2801f2b5643f037dc798b92b1fba
Danizony <danzin2000@example.com>
2022-10-11 14:13:50 -0700

    updated

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -13,15 +13,16 @@
  */
 struct dog
 {
-	char *name;
-	float age;
-	char *owner;
+char *name;
+float age;
+char *owner;
 };
 
 /**
  *dog_t - Typedef for srruct dog
  */
 typedef struct dog dog_t;
+
 
 void init_dog(struct dog *d, char *name, float age, char *owner) 
 void print_dog(struct dog *d);
