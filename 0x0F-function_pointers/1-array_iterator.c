6fe38dc8780e08297ffc00a535431aea16a98869
Danizony <danzin2000@example.com>
2022-10-12 15:48:11 -0700

    update

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -10,14 +10,11 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
+size_t i;
 
-if (array == NULL || action == NULL)
-return;
-
-while (size-- > 0)
+if (array != NULL && action != NULL)
 {
-action(*array);
-array++;
+	for (i = 0; i < size; i++)
+		action(array[i]);
 }
-
 }
