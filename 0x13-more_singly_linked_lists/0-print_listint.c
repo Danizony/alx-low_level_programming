91bd4b141fde07ecd9101a57bbadf31bd3ae0a4c
Danizony <danzin2000@example.com>
2022-10-25 18:50:50 -0700

    correction

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,8 +1,8 @@
 #include "lists.h"
 
 /**
- *print_listint - prints all the elements of a linked list
- *@h: head of the list
+ * print_listint - prints all the elements of a linked list
+ * @h: head of the list
  *
  * Return: the number of nodes
  */
